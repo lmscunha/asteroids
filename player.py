85e8be7fc2a458bc4f3aab74fb2f61318db76ebc
Leonardo Cunha <agile.ice4816@fastmail.com>
2024-10-17 16:05:13 -0300

    fix velocity

--- player.py
+++ player.py
@@ -51,7 +51,6 @@ class Player(circleshape.CircleShape):
         self.position += forward * PLAYER_SPEED * dt
 
     def shoot(self):
-        shot = Shot(self.position, self.position)
-        shot.velocity = pygame.Vector2(0, 1)
-        shot.position.rotate(self.rotation)
-        shot.velocity *= PLAYER_SHOOT_SPEED
+        shot = Shot(self.position.x, self.position.y)
+        shot.velocity = pygame.Vector2(0, 1).rotate(
+            self.rotation) * PLAYER_SHOOT_SPEED
